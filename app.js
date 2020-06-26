95936fb1fd88dbe04a5bd5e51bd23a27d13ef2b6
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-06-26 23:37:07 +0530

    fix db connect

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
 const http = require('http');
 const express = require("express");
+const bodyParser= require('body-parser')
 const path = require("path");
 const app = express();
 const port = process.env.PORT || "8000";
-const {MongoClient} = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
-app.get("/", (req, res) => {
-    res.status(200).send("WHATABYTE: Food For Devs");
-});
+const connectionString =  "mongodb+srv://ayushi:ayushi1234@cluster0-jvytf.mongodb.net/ayushi?retryWrites=true&w=majority"
+const client = new MongoClient(connectionString, { useNewUrlParser: true });
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
-  });
+});
 
-  async function main(){
-    const uri = "mongodb+srv://ayushi:ayushi1234@cluster0-jvytf.mongodb.net/ayushi?retryWrites=true&w=majority";
-    const client = new MongoClient(uri);
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-  }
-  main().catch(console.error);
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("db connected")
+  client.close();
+})
