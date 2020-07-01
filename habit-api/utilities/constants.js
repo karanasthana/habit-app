40c2696d47a4be3da27c6f4f9470813b9ab5533e
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-07-02 00:27:16 +0530

    add constants file

---
+++ habit-api/utilities/constants.js
@@ -0,0 +1,7 @@
+'use strict';
+module.exports = {
+    DB:{
+        NAME: "habit-app",
+        CONNECTION_STRING: "mongodb+srv://ayushi:ayushi1234@cluster0-jvytf.mongodb.net/ayushi?retryWrites=true&w=majority",
+    }
+}
